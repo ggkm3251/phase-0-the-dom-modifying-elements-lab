b83d28c299651ec432b75fe7a68f648330a12f80
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-05 16:11:25 +0300

    commit tree b261de36223c46216c15c569e4aef61ce1d5e51d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+//const newHeader = document.createElement('h1');
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.append(newHeader);
+
+
